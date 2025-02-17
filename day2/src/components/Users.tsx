810c69fa28baf10214f7cb09912bd6702636a45b
aman-pathan-josh <aman.pathan@joshsoftware.com>
2025-02-17 13:32:57 +0530

    Added type User

--- day2/src/components/Users.tsx
+++ day2/src/components/Users.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './style.css';
+import { User } from './types.ts';
 
-const Users = ()=>{
-    const [users,setUsers] = useState([]);
+const Users:React.FC = ()=>{
+    const [users,setUsers] = useState<Array<User>>([]);
 
     const fetchUsers = async ()=>{
         const res = await axios('https://reqres.in/api/users');
@@ -21,7 +22,7 @@ const Users = ()=>{
         <div className='users_div'>
             {users.map((user)=>{
                 return (
-                    <div className='user_card'>
+                    <div className='user_card' key={user?.id}>
                         <img className='user_img' src={user?.avatar}/>
                         <div className='user_details'>
                             <h3>Name: {user?.first_name} {user?.last_name}</h3>
